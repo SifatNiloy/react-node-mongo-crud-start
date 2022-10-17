95dc937b8d1feee40c8fd60c7bb76cdf7d0543f8
unknown <sifatniloy18@gmail.com>
2022-10-17 09:19:01 -0600

    user updated on ui

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -11,10 +11,28 @@ const UpdateUser = () => {
             .then(data => setUser(data))
 
     }, [])
+    const handleNameChange = e => {
+        const updatedName = e.target.value;
+        const updatedUser = { name: updatedName, email: user.email };
+        setUser(updatedUser);
+    }
+    const handleEmailChange = e => {
+        const updatedEmail = e.target.value;
+        const updatedUser = { name: user.name, email: updatedEmail };
+        setUser(updatedUser);
+    }
+    const handleUpdateUser = e => {
+
+    }
     return (
         <div>
-            <h2>update  : {user.name} </h2>
+            <h2>update  : {user.name} {user.email} </h2>
             <p><small>{id}</small></p>
+            <form onSubmit={handleUpdateUser}>
+                <input type="text" onChange={handleNameChange} value={user.name || ""} />
+                <input type="email" onChange={handleEmailChange} value={user.email || ""} />
+                <input type="submit" value="update" />
+            </form>
         </div>
     );
 };
